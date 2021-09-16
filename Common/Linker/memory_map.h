e5cae314143caccaf035487ebf7340f388f1686b
magiczny-kacper <kapibrv97@gmail.com>
2021-09-16 23:34:46 +0200

    Added linker files

---
+++ Common/Linker/memory_map.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern int __boot_flash_start__;
+extern int __boot_flash_size__;
+extern int __app1_flash_start__;
+extern int __app1_flash_size__;
+extern int __app2_flash_start__;
+extern int __app2_flash_size__;
+extern int __swap_start__;
+extern int __swap_size__;
